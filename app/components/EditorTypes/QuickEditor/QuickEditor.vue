93bbcc0a3a1258b303581449c32e8879ac026b1d
le0o0oo <le4ndro.fioretti@gmail.com>
2025-09-09 17:31:43 +0200

    added character inventory editor

--- app/components/EditorTypes/QuickEditor/QuickEditor.vue
+++ app/components/EditorTypes/QuickEditor/QuickEditor.vue
@@ -2,6 +2,7 @@
 import NoEditor from "./interfaces/NoEditor.vue";
 
 import Wallet from "./interfaces/Wallet.vue";
+import CharactersInventory from "./interfaces/CharactersInventory.vue";
 
 const filesStore = useFilesStore();
 
@@ -11,8 +12,13 @@ const currentFileName = computed(() => {
 </script>
 
 <template>
-  <div class="size-full">
+  <div
+    class="size-full max-h-[calc(100vh-140px)] overflow-y-auto overflow-x-hidden"
+  >
     <Wallet v-if="currentFileName === 'wallet.json'" />
+    <CharactersInventory
+      v-else-if="currentFileName === 'characters_inventory.json'"
+    />
 
     <NoEditor v-else @useOther="$emit('useOther')" />
   </div>
