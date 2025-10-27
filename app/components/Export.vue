c0f3cad69a0a8cd1b2ba2c79f7b1539e8da4c34f
loctaz <103637589+loctaz@users.noreply.github.com>
2025-10-27 20:52:49 +0100

    Update Export.vue

--- app/components/Export.vue
+++ app/components/Export.vue
@@ -36,7 +36,7 @@ async function copy() {
   try {
     const result = await exportUtil(exportModal.state.value.fileIndex);
     navigator.clipboard.writeText(result);
-    toast.success("Copied to clipboard");
+    toast.success(""Copié dans le presse-papiers");
   } catch (error) {
     console.error(error);
     toast.error(
